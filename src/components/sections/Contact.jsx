c4720feaa6af447778ccb42917d8614afc6a3b7f
mobasshirCode <mobasshir.code@gmail.com>
2025-02-16 12:01:16 +0530

    email issue refix

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -12,12 +12,14 @@ const Contact = () => {
 
   
 const PUBLIC_KEY = "sLnuXolpeVKjGDKdv";
+const SERVICE_ID = "service_4mdf5nv";
+const TEMPLATE_ID = "template_y2itwqa";
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, e.target, PUBLIC_KEY)
+      .sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
       .then((result) => {
         alert("Message Sent!");
         setFormData({name:"",
